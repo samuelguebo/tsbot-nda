0d94a0ce8bf951fe40927e34bebac11032418b12
Samuel Guebo <samuelguebo@gmail.com>
2019-06-12 18:53:04 +0000

    [IMPR] Up date gitignore

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
 # -*- coding: utf-8 -*-
 #
-# This file is part of the Toolforge Flask + OAuth WSGI tutorial
+# This project is based on the Toolforge Flask + OAuth WSGI tutorial
+# It was tailored to fit the news of the Trust & Safety team
 #
-# Copyright (C) 2017 Bryan Davis and contributors
-#
-# This program is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option)
-# any later version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-# more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Contributors: Samuel Guebo
+# Licence: MIT
 
 import flask
 import mwoauth
